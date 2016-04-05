d45e11a639d4a4a3cc186d03fda5e4735a7485bb
stetteh <stettehq@gmail.com>
2016-04-05 10:01:16 -0500

    Initial Commit

---
+++ StringSplit/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringSplit
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void TestFirstName()
+        {
+            Assert.AreEqual("Daniel", FirstName("Daniel Pollock"));
+        }
+
+
+        [TestMethod]
+        public void TestLastName()
+        {
+            Assert.AreEqual("Pollock", LastName("Daniel Pollock"));
+        }
+
+    
+
+        [TestMethod]
+        public void TestOneWordName()
+        {
+            Assert.AreEqual("Moby", FirstName("Moby"));
+            Assert.AreEqual("Moby", LastName("Moby"));
+        }
+
+
+
+        [TestMethod]
+        public void TestThreeWordName()
+        {
+            Assert.AreEqual("John Quincy", FirstName("John Quincy Adams"));
+            Assert.AreEqual("Adams", LastName("John Quincy Adams"));
+        }
+
+        [TestMethod]
+        public void TestNoWordName()
+        {
+            Assert.AreEqual("", FirstName(""));
+            Assert.AreEqual("", LastName(""));
+        }
+    }
+
+}
